d3435a64c7d4f6ea558175adac2158d0bda8d3a3
Calum Haughton <calumpeterhaughton@gmail.com>
2023-01-19 21:27:04 +0000

    Added checkbox styling

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="card" @click="toggleSelected()">
+    <div class="card" @click="toggleSelected()" :class="{done: check.state}">
         <div class="card-content">
             <div class="content">
-                <div class="columns is-mobile">
-                    <div class="column is-10">
+                <div class="columns is-mobile is-vcentered">
+                    <div class="column is-10" :class="{'text-strikethrough': check.state}">
                         <h3>{{ props.header }}</h3>
                         <p>{{ props.subheader }}</p>
                     </div>
                     <div class="column is-2">
-                        <input type="checkbox" :id="props.id" v-model="check.state"/>
-                        {{ check.state }}
+                        <div class="round">
+                            <input type="checkbox" :id="props.id" v-model="check.state"/>
+                            <label :for="props.id"></label>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -47,5 +49,61 @@ function toggleSelected() {
 <style scoped>
 .card {
     margin-bottom:24px;
+    transition: 0.2s linear;
+}
+
+.done {
+    opacity:0.2;
+}
+
+.text-strikethrough {
+    text-decoration: line-through;
+}
+
+.round {
+  position: relative;
+}
+
+.round label {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  cursor: pointer;
+  height: 28px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 28px;
+}
+
+.round label:after {
+  border: 2px solid #fff;
+  border-top: none;
+  border-right: none;
+  content: "";
+  height: 6px;
+  left: 7px;
+  opacity: 0;
+  position: absolute;
+  top: 8px;
+  transform: rotate(-45deg);
+  width: 12px;
+}
+
+.round input[type="checkbox"] {
+  visibility: hidden;
+}
+
+.round input[type="checkbox"]:checked + label {
+  background-color: #66bb6a;
+  border-color: #66bb6a;
+}
+
+.round input[type="checkbox"]:checked + label:after {
+  opacity: 1;
+}
+
+label {
+    pointer-events: none;
 }
 </style>
